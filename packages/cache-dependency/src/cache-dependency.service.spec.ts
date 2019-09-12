da2fd32f96e9b031b40196afd9080ed0dfcde21f
keigo <keigo@unity3d.com>
2019-09-12 16:41:22 +0900

    test: add CacheDependencyService test

---
+++ packages/cache-dependency/src/cache-dependency.service.spec.ts
@@ -0,0 +1,112 @@
+import { CACHE_MANAGER } from "@nestjs/common";
+import { Test } from "@nestjs/testing";
+import { caching } from "cache-manager";
+import { CacheDependencyGraph } from "./cache-dependency.interface";
+import { CacheDependencyService } from "./cache-dependency.service";
+
+describe("CacheDependencyService", () => {
+  let service: CacheDependencyService;
+  beforeEach(async () => {
+    const app = await Test.createTestingModule({
+      providers: [
+        CacheDependencyService,
+        {
+          provide: CACHE_MANAGER,
+          useValue: caching({
+            store: "memory",
+            max: Number.MAX_SAFE_INTEGER,
+            ttl: 1000,
+          }),
+        },
+      ],
+    }).compile();
+
+    expect(app).toBeDefined();
+    service = app.get<CacheDependencyService>(CacheDependencyService);
+  });
+  it("should be defined", () => {
+    expect(service).toBeDefined();
+  });
+  describe("getCache/setCache/deleteCache", () => {
+    it("should be defined", () => {
+      expect(service.getCache).toBeDefined();
+      expect(service.setCache).toBeDefined();
+      expect(service.deleteCache).toBeDefined();
+    });
+
+    it("test", async () => {
+      await expect(service.getCache("test")).resolves.toBeUndefined();
+      await service.setCache("test", 1);
+      await expect(service.getCache("test")).resolves.toBe(1);
+      await service.deleteCache("test");
+      await expect(service.getCache("test")).resolves.toBeUndefined();
+    });
+  });
+
+  describe("dependency", () => {
+    it("should clear A and A-A", async () => {
+      await service.createCacheDependencies((graph: CacheDependencyGraph) => {
+        graph.addNode("A", 1);
+        graph.addNode("A-A");
+        graph.addNode("A-B");
+        graph.addDependency("A-A", "A");
+        graph.addDependency("A-B", "A");
+      });
+      await expect(service.getKeys()).resolves.toEqual(["cache-dependency:A-B", "cache-dependency:A-A", "A"]);
+
+      await service.clearCacheDependencies("A-A");
+
+      await expect(service.getKeys()).resolves.toEqual(["cache-dependency:A-B"]);
+    });
+
+    it("should clear A and A-A and A-A-A", async () => {
+      await service.createCacheDependencies((graph: CacheDependencyGraph) => {
+        graph.addNode("A", 1);
+        graph.addNode("A-A");
+        graph.addNode("A-B");
+        graph.addNode("A-A-A");
+        graph.addDependency("A-A", "A");
+        graph.addDependency("A-A-A", "A-A");
+        graph.addDependency("A-B", "A");
+      });
+
+      await expect(service.getKeys()).resolves.toEqual([
+        "cache-dependency:A-A-A",
+        "cache-dependency:A-A",
+        "cache-dependency:A-B",
+        "A",
+      ]);
+
+      await service.clearCacheDependencies("A-A-A");
+
+      await expect(service.getKeys()).resolves.toEqual(["cache-dependency:A-B"]);
+    });
+
+    it("should clear all", async () => {
+      await service.createCacheDependencies((graph: CacheDependencyGraph) => {
+        graph.addNode("A", 1);
+        graph.addNode("A-A");
+        graph.addNode("A-B");
+        graph.addNode("A-B-A");
+        graph.addNode("A-A-A");
+        graph.addDependency("A-A", "A");
+        graph.addDependency("A-A-A", "A-A");
+        graph.addDependency("A-B", "A");
+        graph.addDependency("A-B-A", "A-B");
+        graph.addDependency("A-B-A", "A-A-A");
+      });
+
+      await expect(service.getKeys()).resolves.toEqual([
+        "cache-dependency:A-B-A",
+        "cache-dependency:A-A-A",
+        "cache-dependency:A-A",
+        "cache-dependency:A-B",
+        "A",
+      ]);
+
+      await service.clearCacheDependencies("A-B-A");
+
+      await expect(service.getKeys()).resolves.toEqual([]);
+    });
+  });
+});
