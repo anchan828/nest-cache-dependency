de288d1ffc8948e2ab75531921d2b829a5b74598
anchan828 <anchan828@gmail.com>
2019-09-18 15:20:19 +0900

    chore(cache-common): remove get function
    nest v6.7.0 already fixed

--- packages/cache-common/src/interfaces.ts
+++ packages/cache-common/src/interfaces.ts
@@ -1,8 +1,7 @@
 import { CacheStore } from "@nestjs/common";
 
-export type CacheManager = Omit<CacheStore, "get"> & {
+export type CacheManager = CacheStore & {
   name: string;
-  get<T>(key: string): Promise<T | undefined>;
   keys(pattern?: string): Promise<string[]>;
   reset(): Promise<void>;
   mget<T>(keys: string[]): Promise<Array<T | undefined>>;
