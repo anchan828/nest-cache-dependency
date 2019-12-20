2f693fc53e4b7e33478b98f52d03830d843c8766
anchan828 <anchan828@gmail.com>
2019-12-21 01:08:53 +0900

    refactor: add return types

--- packages/cache-dependency/src/cache-dependency.decorator.ts
+++ packages/cache-dependency/src/cache-dependency.decorator.ts
@@ -1,8 +1,8 @@
 import { SetMetadata } from "@nestjs/common";
 import { CacheDependencyFunction } from "./cache-dependency.interface";
 import { CACHE_DEPENDENCY_KEY_METADATA, CLEAR_CACHE_DEPENDENCIES_KEY_MATADATA } from "./constants";
-export const CacheDependency = <T>(func: CacheDependencyFunction<T>): Function =>
+export const CacheDependency = <T>(func: CacheDependencyFunction<T>): MethodDecorator =>
   SetMetadata(CACHE_DEPENDENCY_KEY_METADATA, func);
 
-export const ClearCacheDependencies = (...cacheKeys: string[]): Function =>
+export const ClearCacheDependencies = (...cacheKeys: string[]): MethodDecorator =>
   SetMetadata(CLEAR_CACHE_DEPENDENCIES_KEY_MATADATA, cacheKeys);
