f500dee11d5f1dbfd91fdc8140063dc5a6ccca4c
Keigo Ando <anchan828@gmail.com>
2022-01-19 10:39:52 +0900

    fix: add unknown first

--- packages/cache-dependency/src/cache-dependency.emitter.ts
+++ packages/cache-dependency/src/cache-dependency.emitter.ts
@@ -2,10 +2,10 @@ import { Injectable } from "@nestjs/common";
 import { EventEmitter } from "events";
 import TypedEmitter from "typed-emitter";
 
-interface CacheDependencyMessageEvents {
+type CacheDependencyMessageEvents = {
   deleted: (keys: string[]) => void;
   delete: (keys: string[]) => void;
-}
+};
 
 @Injectable()
-export class CacheDependencyEventEmitter extends (EventEmitter as new () => TypedEmitter<CacheDependencyMessageEvents>) {}
+export class CacheDependencyEventEmitter extends (EventEmitter as unknown as new () => TypedEmitter<CacheDependencyMessageEvents>) {}
