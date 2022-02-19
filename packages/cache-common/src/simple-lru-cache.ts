e800e4222a0286107114e97da3e4577a34b22c3d
Keigo Ando <anchan828@gmail.com>
2022-02-20 03:59:44 +0900

    fix: export

--- packages/cache-common/src/simple-lru-cache.ts
+++ packages/cache-common/src/simple-lru-cache.ts
@@ -2,5 +2,6 @@ import * as LRUCache from "lru-cache";
 import { Options, SetOptions } from "lru-cache";
 
 export const SimpleLRUCache = LRUCache;
+export type SimpleLRUCache<K, V> = LRUCache<K, V>;
 export type SimpleLRUCacheOptions<K, V> = Options<K, V>;
 export type SimpleLRUCacheSetOptions<V> = SetOptions<V>;
