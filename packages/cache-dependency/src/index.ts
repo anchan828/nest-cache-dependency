4a429f4ff1f6e400dd84290ce4598e7309f8f3a9
Keigo Ando <anchan828@gmail.com>
2022-02-20 03:53:21 +0900

    fix: export

--- packages/cache-dependency/src/index.ts
+++ packages/cache-dependency/src/index.ts
@@ -1,3 +1,9 @@
+export {
+  CACHE_DEPENDENCY_PREFIX_CACHE_KEY,
+  SimpleLRUCache,
+  SimpleLRUCacheOptions,
+  SimpleLRUCacheSetOptions,
+} from "@anchan828/nest-cache-common";
 export { CacheDependency } from "./cache-dependency.decorator";
 export { CacheDependencyInterceptor } from "./cache-dependency.interceptor";
 export {
@@ -10,4 +16,3 @@ export {
 } from "./cache-dependency.interface";
 export { CacheDependencyModule } from "./cache-dependency.module";
 export { CacheDependencyService } from "./cache-dependency.service";
-export { CACHE_DEPENDENCY_PREFIX_CACHE_KEY } from "@anchan828/nest-cache-common";
